7c3316108292a8f4d37f416da6a6fed1d1d1dfd4
Anubhuti <40819989+anubhutisinha04@users.noreply.github.com>
2026-02-22 23:59:28 -0500

    useState with prev

--- src/App.js
+++ src/App.js
@@ -1,72 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
-import Product from './components/Product';
-import Employees from './components/Employees';
+import logo from "./logo.svg";
+import "./App.css";
+import Product from "./components/Product";
+import Employees from "./components/Employees";
+import { useState } from "react";
 
-
-    const products = [
-
-        {
-        code: '0001',
-        name: 'Pineapple',
-        price: 12,
-        image: '/pineapple.jpg',
-        isActive: true
-        },
-        {
-        code: '0002',
-        name: 'Pear',
-        price: 10,
-        image: '/pear.jpg',
-        isActive: false
-        }
-        ,
-        {        
-        code: '0003',
-        name: 'Strawberry',
-        price: 16,
-        image: '/strawberry.jpg',
-        isActive: true
-        }
-
-]
+const products = [
+  {
+    code: "0001",
+    name: "Pineapple",
+    price: 12,
+    image: "/pineapple.jpg",
+    isActive: true,
+  },
+  {
+    code: "0002",
+    name: "Pear",
+    price: 10,
+    image: "/pear.jpg",
+    isActive: false,
+  },
+  {
+    code: "0003",
+    name: "Strawberry",
+    price: 16,
+    image: "/strawberry.jpg",
+    isActive: true,
+  },
+];
 
 const employees = [
   {
-    id: 'E001',
-    name: 'John Doe',
-    position: 'Software Engineer',
-    department: 'IT',
+    id: "E001",
+    name: "John Doe",
+    position: "Software Engineer",
+    department: "IT",
     salary: 75000,
-    active: true
+    active: true,
   },
   {
-    id: 'E002',
-    name: 'Jane Smith',
-    position: 'Project Manager',
-    department: 'Operations',
+    id: "E002",
+    name: "Jane Smith",
+    position: "Project Manager",
+    department: "Operations",
     salary: 85000,
-    active: true
+    active: true,
   },
   {
-    id: 'E003', 
-    name: 'Alice Johnson',
-    position: 'Data Analyst',  
-    department: 'Marketing',
+    id: "E003",
+    name: "Alice Johnson",
+    position: "Data Analyst",
+    department: "Marketing",
     salary: 68000,
-    active: false
+    active: false,
   },
   {
-    id: 'E004',
-    name: 'Bob Brown',
-    position: 'HR Manager',
-    department: 'Human Resources',
+    id: "E004",
+    name: "Bob Brown",
+    position: "HR Manager",
+    department: "Human Resources",
     salary: 102000,
-    active: true
+    active: true,
+  },
+];
+
+const inititalizeArr = () => {
+  const arrList = [];
+
+  for (let i = 0; i < 5; i++) {
+    arrList.push(`Item ${i + 1}`);
   }
-]
+  return arrList;
+};
 
 function App() {
+  const updateArr = (action) => {
+    if (action === "add") {
+      setArr((prev) => [...arr, `Item ${arr.length + 1}`]);
+    }
+
+    if (action === "rem") {
+      setArr((prev) => prev.slice(0, -1));
+    }
+  };
+
+  const [arr, setArr] = useState(inititalizeArr);
+
+  console.log(arr);
 
   const calculateFactorial = (n) => {
     let factorial = 1;
@@ -74,37 +93,34 @@ function App() {
       factorial *= i;
     }
     console.log(factorial);
-  }
+  };
 
   return (
-  <>
-   
-    <h1>Employees</h1>
-    <div className='employee-list'>
+    <>
+      <h1>Employees</h1>
+      <div className="employee-list">
+        {employees.map((employee) => {
+          return employee.active && <Employees employee={employee} />;
+        })}
+      </div>
 
-      {
-        employees.map((employee) => {
+      <button onClick={() => calculateFactorial(9)}>Factorial</button>
 
-         return employee.active && <Employees employee={employee}/>
-
-       })
-      }
-
-    </div>
-
-    <button onClick={() => calculateFactorial(9)}>Factorial</button>
-
-
-
-   </>
+      <div>
+        {arr.map((item) => {
+          return <h3 key={item}>{item}</h3>;
+        })}
+      </div>
+      <button onClick={() => updateArr("add")}>Add</button>
+      <button onClick={() => updateArr("rem")}>Remove</button>
+    </>
   );
 }
 
 export default App;
 
-
-
-{/* <h1>Products</h1>
+{
+  /* <h1>Products</h1>
    <div className='product-list'>
    {
     products.map((product) => {
@@ -113,4 +129,5 @@ export default App;
    }
    
    </div>
-   <button onClick={() => handleClick("Hello!")}>Click Me</button> */}
+   <button onClick={() => handleClick("Hello!")}>Click Me</button> */
+}
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,17 @@
 const Product = ({ product }) => {
-
-    return (
-         <div className="card">
-            <img src={product.image} alt={`picture of a ${product.name.toLowerCase()}`} height={120} width={120}/>
-            <span>{product.code}</span>
-            <span>{product.name}</span>
-            <span>${product.price}</span>
-        </div>
-       );
-    
-}
+  return (
+    <div className="card">
+      <img
+        src={product.image}
+        alt={`picture of a ${product.name.toLowerCase()}`}
+        height={120}
+        width={120}
+      />
+      <span>{product.code}</span>
+      <span>{product.name}</span>
+      <span>${product.price}</span>
+    </div>
+  );
+};
 
 export default Product;
